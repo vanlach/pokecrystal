96f9722c9e9e9abc64d0a181cf8698d62aa37693
vanlach <evantarlach@Evans-MacBook-Pro.local>
2020-02-19 20:49:55 -0600

    Print Text When You Lose a Trainer Battle

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -2908,9 +2908,15 @@ LostBattle:
 	bit 0, a
 	jr nz, .battle_tower
 
-	ld a, [wBattleType]
-	cp BATTLETYPE_CANLOSE
-	jr nz, .not_canlose
+	ld a, [wBattleMode]
+	dec a ; wild?
+	jr z, .no_loss_text
+
+	ld hl, wLossTextPointer
+	ld a, [hli]
+	ld h, [hl]
+	and h
+	jr z, .no_loss_text
 
 ; Remove the enemy from the screen.
 	hlcoord 0, 0
@@ -2946,7 +2952,7 @@ LostBattle:
 	call ClearBGPalettes
 	ret
 
-.not_canlose
+.no_loss_text
 	ld a, [wLinkMode]
 	and a
 	jr nz, .LostLinkBattle
