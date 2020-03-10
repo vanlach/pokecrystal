b26ea051b49996ba864836f3a078b70250a34811
vanlach <evantarlach@Evans-MacBook-Pro.local>
2020-03-09 20:24:46 -0500

    Tweaked Wild Pokémon Probabilities

--- data/wild/probabilities.asm
+++ data/wild/probabilities.asm
@@ -4,15 +4,15 @@ mon_prob: MACRO
 ENDM
 
 GrassMonProbTable:
-	mon_prob 30,  0 ; 30% chance
-	mon_prob 60,  1 ; 30% chance
-	mon_prob 80,  2 ; 20% chance
-	mon_prob 90,  3 ; 10% chance
-	mon_prob 95,  4 ;  5% chance
-	mon_prob 99,  5 ;  4% chance
-	mon_prob 100, 6 ;  1% chance
+	mon_prob 25,  0 ; 25% chance
+	mon_prob 45,  1 ; 20% chance
+	mon_prob 60,  2 ; 15% chance
+	mon_prob 75,  3 ; 15% chance
+	mon_prob 85,  4 ; 10% chance
+	mon_prob 95,  5 ; 10% chance
+	mon_prob 100, 6 ;  5% chance
 
 WaterMonProbTable:
-	mon_prob 60,  0 ; 60% chance
-	mon_prob 90,  1 ; 30% chance
-	mon_prob 100, 2 ; 10% chance
+	mon_prob 50,  0 ; 50% chance
+	mon_prob 85,  1 ; 35% chance
+	mon_prob 100, 2 ; 15% chance
